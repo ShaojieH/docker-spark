545302bd36a7e3afdd2f4f4a991acc33a9baa0a8
houshaojie <tomhou@buaa.edu.cn>
2020-01-16 16:39:19 +0800

    fix

--- javatc.sh
+++ javatc.sh
@@ -1,5 +1,5 @@
 while true
 do
-  sudo docker exec -d docker-spark_master_1 bin/run-example SparkPi 10 
-  sleep 10
+  sudo docker exec -it  docker-spark_master_1 bin/run-example JavaTC
+  sleep 30
 done
